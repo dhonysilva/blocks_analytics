e9c2b24450e858bb0dbf05d3d2c70a5b9794152d
Dhony Silva <dhony.web@gmail.com>
2025-07-03 21:32:01 -0300

    Update Migrations.AddTemperatureTable with required funcions - not working yet

--- priv/clickhouse_repo/migrations/20250702190831_add_temperature_table.exs
+++ priv/clickhouse_repo/migrations/20250702190831_add_temperature_table.exs
@@ -1,7 +1,23 @@
 defmodule BlocksAnalytics.ClickhouseRepo.Migrations.AddTemperatureTable do
   use Ecto.Migration
 
-  def change do
+  def up do
+    create_temperatures()
+  end
 
+  defp create_temperatures() do
+    create_if_not_exists table(:teperatures,
+                           primary_key: false,
+                           engine: "MergeTree",
+                           options: """
+                           PARTITION BY toYYYYMM(timestamp)
+                           ORDER BY (device_id, toDate(timestamp))
+                           #{BlocksAnalytics.MigrationUtils.table_settings_expr()}
+                           """
+                         ) do
+      add(:device_id, :UInt64)
+      add(:value, :integer)
+      add(:timestamp, :naive_datetime)
+    end
   end
 end
