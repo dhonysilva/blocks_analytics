f4fe43e380a5836e749ad2778631e5786823cccf
Dhony Silva <dhony.web@gmail.com>
2025-07-03 21:30:58 -0300

    adding missing Plausible funcions into ClickhouseRepo required by MigrationUltils

--- lib/blocks_analytics/clickhouse_repo.ex
+++ lib/blocks_analytics/clickhouse_repo.ex
@@ -2,4 +2,23 @@ defmodule BlocksAnalytics.ClickhouseRepo do
   use Ecto.Repo,
     otp_app: :blocks_analytics,
     adapter: Ecto.Adapters.ClickHouse
+
+  defmacro __using__(_) do
+    quote do
+      alias Plausible.IngestRepo
+      import Ecto
+      import Ecto.Query, only: [from: 1, from: 2]
+    end
+  end
+
+  def clustered_table?(table) do
+    replica_count(table) > 1
+  end
+
+  def replica_count(table) do
+    {:ok, %{rows: [[count]]}} =
+      query("SELECT sum(active_replicas) FROM system.replicas WHERE table = '#{table}'")
+
+    count
+  end
 end
